2acb32f1b97bdef09d04d359351c7079b4b7ae17
MarketingComunycarse <comunycarse.mktg@gmail.com>
2022-05-10 17:39:07 +0200

    update

--- script.js
+++ script.js
@@ -234,6 +234,12 @@ var sendLeadToCRM = function () { return __awaiter(void 0, void 0, void 0, funct
         }
     });
 }); };
+firebase.initializeApp({
+    apiKey: "AIzaSyCzHO8J6xRErJST5YDEuE8cN_6m-EpLRTo",
+    authDomain: "lead-manager-pozuelo.firebaseapp.com",
+    projectId: "lead-manager-pozuelo",
+});
+var db = firebase.firestore();
 var dataForm = {};
 var timestamp, today, month, year, source, currentUrl;
 var sendLeadToDataBase = function () {
@@ -242,12 +248,35 @@ var sendLeadToDataBase = function () {
     month = new Date().toLocaleString('en-US', { month: 'long', timeZone: 'Europe/Madrid' });
     year = new Date().toLocaleString('en-US', { year: 'numeric', timeZone: 'Europe/Madrid' });
     var leadToDB = __assign(__assign({}, lead), { status: 0, createDate: today, timestamp: timestamp, source: domain });
-    console.log('lead to db => ', leadToDB);
-    goThanksPage();
+    db.collection("leads-".concat(year, "-").concat(month)).add(leadToDB).then(function () {
+        goThanksPage();
+    }).catch(function (err) { return console.error(err); });
 };
 var goThanksPage = function () {
+    var websitesWithThanksPage = ['ecomfax', 'recordia'];
     var isDownload = document.querySelector('#download-form') || false;
-    console.log('lead => ', lead);
+    websitesWithThanksPage.forEach(function (web) {
+        if (domain.includes(web)) {
+            if (isDownload) {
+                if (pathname.includes("/es/")) {
+                    window.location.href = "/es/gracias-download/";
+                }
+                else {
+                    window.location.href = "/en/thanks-download/";
+                }
+                ;
+            }
+            else {
+                if (pathname.includes("/es/")) {
+                    window.location.href = "/es/gracias/";
+                }
+                else {
+                    window.location.href = "/en/thanks/";
+                }
+                ;
+            }
+        }
+    });
 };
 window.onload = function () {
     setSessionlandingPage();
